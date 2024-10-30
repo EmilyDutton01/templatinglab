156fc4206d35e615b8ca8b28f0cc9d5f90a651bb
EmilyDutton01 <edutton01@aurora.edu>
2024-10-30 13:04:54 -0500

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,28 @@
+interface Ingredient{
+    void getName();
+    void getQuantity();
+}
+class SolidIngredient implements Ingredient{
+    private String _name;
+    private double _quantity;
+    public SolidIngredient(String n, double qt){
+        _name = n;
+        _quantity = qt;
+    }
+    public void getName(){String n = _name;}
+    public void getQuantity(){ double qt = _quantity;}
+}
+class LiquidIngredient implements Ingredient{
+    private String _name;
+    private double _quantity;
+    public LiquidIngredient(String n, double qt){
+        _name = n;
+        _quantity = qt;
+    }
+    public void getName(){ String n =_name;}
+    public void getQuantity(){ double qt =_quantity;}
+}
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
